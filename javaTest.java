f433bf8b05a26a11f0a74df7dcb53914c3f8bd3f
Anup Jacob <L00163455@student.lyit.ie>
2021-09-29 10:36:36 +0100

    First push to commit by Anup

--- javaTest.java
+++ javaTest.java
@@ -1,6 +1,1 @@
-public class HelloLYIT {
-    public static void main(String[] args) {
-        // Prints "Hello, LYIT from Ruth" as output
-        System.out.println("Hello, LYIT from Ruth");
-    }
-}
+
